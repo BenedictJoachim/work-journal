016606aebb887797652409c9bca3c92e56f6f16e
BenedictJoachim <148780517+BenedictJoachim@users.noreply.github.com>
2024-08-08 09:39:32 +0000

    Build the entry form

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -16,22 +16,59 @@ export default function Index() {
     <div className="mt-4">
       <p className="font-bold">Week of February 20<sup>th</sup></p>
     </div>
-    <div className="mt-4">
-      <div className="mt-3 space-y-4">
+
+    <div className="my-8 border p-2">
+      <form>
+        <p className="italic">Create an entry</p>
+
+        <div className="mt-4">
+          <div>
+            <input type="date" name="date" className="text-gray-700" />
+          </div>
+
+          <div className="mt-2 sapce-x-8">
+            <label>
+              <input className="mr-0" type="radio" name="category" value="work"/>
+              Work
+            </label>
+            <label>
+              <input className="ml-4" type="radio" name="category" value="learnings"/>
+              Learnings
+            </label>
+            <label>
+              <input className="ml-4" type="radio" name="category" value="intresting_things"/>
+              Intresting thing
+            </label>
+          </div>
+
+          <div className="mt-2">
+              <textarea name="" className="w-full text-gray-700" placeholder="Write your entry..." />
+          </div>
+
+          <div className="mt-2 text-right">
+            <button className="py-1 px-4 font-medium bg-blue-500 text-white" type="submit">Save</button>
+          </div>
+        </div>
+      </form>
+    </div>
+
+
+    <div className="mt-4 space-y-4">
+      <div className="mt-3">
         <p>Work</p>
         <ul className="ml-8 list-disc">
           <li>First item</li>
           <li>Second item</li>
         </ul>
       </div>
-      <div>
+      <div className="mt-3">
         <p>Learnings</p>
         <ul className="ml-8 list-disc">
           <li>First item</li>
           <li>Second item</li>
         </ul>
       </div>
-      <div>
+      <div className="mt-3">
         <p>Intresting things</p>
         <ul className="ml-8 list-disc">
           <li>First item</li>
