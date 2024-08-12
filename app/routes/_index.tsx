982b06cdb102145ebe69fb865948eba887010586
BenedictJoachim <148780517+BenedictJoachim@users.noreply.github.com>
2024-08-12 07:41:35 +0000

    Enhanced the form to be disabled while submitting to prevent duplicates and added default current date value with date-fns format

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -2,7 +2,7 @@ import { Form, useFetcher } from "@remix-run/react";
 import { redirect } from "@remix-run/node";
 import type { ActionFunctionArgs, MetaFunction } from "@remix-run/node";
 import { PrismaClient } from '@prisma/client'
-
+import { format } from "date-fns";
 
 export const meta: MetaFunction = () => {
   return [
@@ -52,36 +52,35 @@ export default function Index() {
       <fetcher.Form method="post">
         <p className="italic">Create an entry</p>
 
-        <div className="mt-4">
-          <div>
-            <input required type="date" name="date" className="text-gray-700" />
+        <fieldset className="disabled:opacity-80" disabled={fetcher.state === "submitting"}>
+          <div className="mt-4">
+            <div>
+              <input required type="date" name="date" defaultValue={format(new Date, "yyyy-MM-dd")} className="text-gray-700" />
+            </div>
+            <div className="mt-2 sapce-x-8">
+              <label>
+                <input required className="mr-0" type="radio" name="type" value="work"/>
+                Work
+              </label>
+              <label>
+                <input className="ml-4" type="radio" name="type" value="learnings"/>
+                Learnings
+              </label>
+              <label>
+                <input className="ml-4" type="radio" name="type" value="intresting-things"/>
+                Intresting thing
+              </label>
+            </div>
+            <div className="mt-2">
+                <textarea required name="text" className="w-full text-gray-700" placeholder="Write your entry..." />
+            </div>
+            <div className="mt-2 text-right">
+              <button className="py-1 px-4 font-medium bg-blue-500 text-white" type="submit">
+                {fetcher.state === "submitting" ? "Saving..." : "Save"}
+              </button>
+            </div>
           </div>
-
-          <div className="mt-2 sapce-x-8">
-            <label>
-              <input required className="mr-0" type="radio" name="type" value="work"/>
-              Work
-            </label>
-            <label>
-              <input className="ml-4" type="radio" name="type" value="learnings"/>
-              Learnings
-            </label>
-            <label>
-              <input className="ml-4" type="radio" name="type" value="intresting-things"/>
-              Intresting thing
-            </label>
-          </div>
-
-          <div className="mt-2">
-              <textarea required name="text" className="w-full text-gray-700" placeholder="Write your entry..." />
-          </div>
-
-          <div className="mt-2 text-right">
-            <button className="py-1 px-4 font-medium bg-blue-500 text-white" type="submit">
-              {fetcher.state === "submitting" ? "Saving..." : "Save"}
-            </button>
-          </div>
-        </div>
+        </fieldset>
       </fetcher.Form>
     </div>
 
